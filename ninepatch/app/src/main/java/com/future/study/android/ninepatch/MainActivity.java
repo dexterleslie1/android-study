80be147cfa5ccc51349d564984ef2672d3471771
dexterleslie@gmail.com <dexterleslie@gmail.com>
2019-08-11 21:52:17 +0800

    新增ninepatch demo

---
+++ ninepatch/app/src/main/java/com/future/study/android/ninepatch/MainActivity.java
@@ -0,0 +1,78 @@
+package com.future.study.android.ninepatch;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        TextView view = findViewById(R.id.textView1);
+        StringBuilder builder = new StringBuilder();
+        for(int i=0; i<10; i++) {
+            builder.append(UUID.randomUUID().toString());
+        }
+        view.setText(builder.toString());
+
+        view = findViewById(R.id.textView2);
+        view.setText(UUID.randomUUID().toString());
+
+        view = findViewById(R.id.textView3);
+        builder = new StringBuilder();
+        for(int i=0; i<10; i++) {
+            builder.append(UUID.randomUUID().toString());
+        }
+        view.setText(builder.toString());
+
+        view = findViewById(R.id.textView4);
+        view.setText(UUID.randomUUID().toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

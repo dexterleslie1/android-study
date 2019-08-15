9c7ad98992b413908e05613e6b032942cc69b946
dexterleslie@gmail.com <dexterleslie@gmail.com>
2019-08-15 13:25:37 +0800

    新增tablayout+viewpager+fragment demo

---
+++ tablayout+viewpager+fragment/app/src/main/java/com/future/study/android/tablayoutviewpagerfragment/MainActivity.java
@@ -0,0 +1,104 @@
+package com.future.study.android.tablayoutviewpagerfragment;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        final List<MyBaseFragment> fragments = new ArrayList<>();
+        MyBaseFragment fragment = new FragmentUnpay();
+        fragments.add(fragment);
+        fragment = new FragmentPaying();
+        fragments.add(fragment);
+        fragment = new FragmentPaymentRecord();
+        fragments.add(fragment);
+
+        MyFragmentPagerAdapter fragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        fragmentPagerAdapter.setFragments(fragments);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        for(int i=0; i<tabLayout.getTabCount(); i++) {
+            if(i == 0) {
+                tabLayout.getTabAt(i).setIcon(fragments.get(i).getIconActive());
+            }else {
+                tabLayout.getTabAt(i).setIcon(fragments.get(i).getIconInactive());
+            }
+        }
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                MyBaseFragment fragment = fragments.get(position);
+                tab.setIcon(fragment.getIconActive());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                MyBaseFragment fragment = fragments.get(position);
+                tab.setIcon(fragment.getIconInactive());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                MyBaseFragment fragment = fragments.get(position);
+                tab.setIcon(fragment.getIconActive());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

c64e5eea0eb45450a7e9f2aa49853082601dcb1c
dexterleslie@gmail.com <dexterleslie@gmail.com>
2019-09-10 23:24:35 +0800

    新增activitylifecyclecallbacks demo

---
+++ activitylifecyclecallbacks/app/src/main/java/com/future/study/android/activitylifecyclecallbacks/BaseApplication.java
@@ -0,0 +1,57 @@
+package com.future.study.android.activitylifecyclecallbacks;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ *
+ */
+public class BaseApplication extends Application implements Application.ActivityLifecycleCallbacks{
+    private final static String TAG = BaseApplication.class.getSimpleName();
+
+    private int activityCount = 0;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.registerActivityLifecycleCallbacks(this);
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+        this.activityCount++;
+        Log.i(TAG, "onActivityStarted activity count:" + activityCount);
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+        this.activityCount--;
+        Log.i(TAG, "onActivityStopped activity count:" + activityCount);
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+    }
+}
